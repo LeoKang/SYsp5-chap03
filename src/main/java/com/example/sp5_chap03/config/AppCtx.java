0477a1dba4a0e7cf89324ec29874fe25f1ad5833
SYU <xeungjin@gmail.com>
2025-03-27 10:23:49 +0900

    ~ 77 page, 스프링을 이용한 객체 조립과 사용

---
+++ src/main/java/com/example/sp5_chap03/config/AppCtx.java
@@ -0,0 +1,28 @@
+package com.example.sp5_chap03.config;
+
+import com.example.sp5_chap03.spring.ChangePasswordService;
+import com.example.sp5_chap03.spring.MemberDao;
+import com.example.sp5_chap03.spring.MemberRegisterService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppCtx {
+
+    @Bean
+    public MemberDao memberDao() {
+        return new MemberDao();
+    }
+
+    @Bean
+    public MemberRegisterService memberRegSvc() {
+        return new MemberRegisterService(memberDao());
+    }
+
+    @Bean
+    public ChangePasswordService changePwdSvc() {
+        ChangePasswordService pwdSvc = new ChangePasswordService();
+        pwdSvc.setMemberDao(memberDao());
+        return pwdSvc;
+    }
+}
